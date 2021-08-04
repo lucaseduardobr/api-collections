3b4bb3d661328f844314249dd20a1f6f2b5579da
boss <lucaseduardocoelho@gmail.com>
2021-08-04 19:41:43 -0300

    Operations with groupBy

--- src/main/kotlin/one/digitalinnovation/collections/TestList.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TestList.kt
@@ -1,18 +1,38 @@
 package one.digitalinnovation.collections
 
 fun main(){
-    val joao=Funcionario(nome="joao",salario = 1000.0)
-    val maria=Funcionario(nome="maria",salario = 2000.0)
-    val rebeca=Funcionario(nome="rebeca",salario = 4000.0)
+    val joao=Funcionario(nome="joao",salario = 1000.0,"CLT")
+    val maria=Funcionario(nome="maria",salario = 8000.0,"CLT")
+    val rebeca=Funcionario(nome="rebeca",salario = 4000.0, "PJ")
     val funcionarios = listOf(joao,maria,rebeca)
     funcionarios.forEach { println(it) }
+
+    println("-----------------------")
+
     println(funcionarios.find { it.nome =="maria" })
+
+    println("-----------------------")
+
+
+    funcionarios
+        .groupBy { it.tipoDeContratacao } // serve pra agrupar dois tipos
+        .forEach { println(it) }
+
+        /*
+    funcionarios
+        .sortedBy { it.salario } serve pra ordenar
+        .forEach { println(it) }
+
+
+
+    */
 }
 //quando vem pegar os dados de funcionario para imrpimir
 //eles pegam esse dados e imprimem como na override function
 data class Funcionario(
     val nome:String,
-    val salario:Double
+    val salario:Double,
+    val tipoDeContratacao:String
 ) {
     override fun toString(): String =
         """
---
+++ src/main/kotlin/one/digitalinnovation/collections/TestSet.kt
@@ -0,0 +1,5 @@
+package one.digitalinnovation.collections
+
+fun main(){
+    val joao = Funcionario("Joao")
+}
